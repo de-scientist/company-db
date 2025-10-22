ac069590f67276d6e744189512144bdfcfa06aef
de-scientist <gitaumark502@gmail.com>
2025-10-22 12:37:59 +0300

    update employee fields

--- indexE.js
+++ indexE.js
@@ -1,14 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
     //Write a function named createEmployee that adds a new employee record to the database. It should take parameters for the id: a string that uniquely identifies an employee firstName: The employee's first name.lastName: The employee's last name..email: The employee's email address. salary: The employee's salary amount.departmentId: The ID of the department the employee belongs to. The function should use Prisma's create method to insert new employee records into the database. 
-    async function createEmployee(id, firstName, lastName, email, salary, departmentId) {
+    async function createEmployee(ID, firstName, lastName, email, salary, departmentId) {
       const employee = await prisma.employee.create({
         data: {
-          id: id,
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-          salary: salary,
-          departmentId: departmentId,
+          ID: ID,
+          FirstName: firstName,
+          LastName: lastName,
+          Email: email,
+          Salary: salary,
+          DepartmentID: departmentId,
         },
       });
       return employee;
