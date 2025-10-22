b82c46218fade212c38b7a9255ad1208ffb3ea49
de-scientist <gitaumark502@gmail.com>
2025-10-22 10:51:07 +0300

    do the first migration to add model department and employees

---
+++ prisma/migrations/20251022074837_add_department_and_employee_models/migration.sql
@@ -0,0 +1,42 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- CreateTable
+CREATE TABLE [dbo].[Departments] (
+    [ID] NVARCHAR(1000) NOT NULL,
+    [DepartmentName] NVARCHAR(50) NOT NULL,
+    [Location] NVARCHAR(100),
+    CONSTRAINT [Departments_pkey] PRIMARY KEY CLUSTERED ([ID])
+);
+
+-- CreateTable
+CREATE TABLE [dbo].[Employees] (
+    [ID] NVARCHAR(1000) NOT NULL,
+    [FirstName] NVARCHAR(50) NOT NULL,
+    [LastName] NVARCHAR(50) NOT NULL,
+    [Email] NVARCHAR(100) NOT NULL,
+    [Salary] FLOAT NOT NULL,
+    [DepartmentID] NVARCHAR(50) NOT NULL,
+    CONSTRAINT [Employees_pkey] PRIMARY KEY CLUSTERED ([ID]),
+    CONSTRAINT [Employees_Email_key] UNIQUE NONCLUSTERED ([Email])
+);
+
+-- CreateIndex
+CREATE NONCLUSTERED INDEX [IX_Employee_DepartmentID] ON [dbo].[Employees]([DepartmentID]);
+
+-- AddForeignKey
+ALTER TABLE [dbo].[Employees] ADD CONSTRAINT [Employees_DepartmentID_fkey] FOREIGN KEY ([DepartmentID]) REFERENCES [dbo].[Departments]([ID]) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
