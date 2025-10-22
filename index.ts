1f8aebc0cd5a8ce18ff7ac63bfeb5bd7138981bd
de-scientist <gitaumark502@gmail.com>
2025-10-22 11:29:06 +0300

    use Prisma's create method to insert a new record into the database

--- index.ts
+++ index.ts
@@ -13,3 +13,14 @@ async function createDepartment(ID: string, departmentName: string, location: st
   });
   return department;
 }
+
+//use Prisma's create method to insert a new record into the database
+async function main() {
+  const newDepartment = await createDepartment("DPT001", "Computer Science", "Nairobi Campus");
+createDepartment("DPT002", "Information Technology", "Mombasa Campus");
+createDepartment("DPT003", "Software Engineering", "Kisumu Campus");
+createDepartment("DPT004", "Cyber Security", "Eldoret Campus");
+createDepartment("DPT005", "Business Studies", "Nakuru Campus");
+createDepartment("DPT006", "Business and Finance", "Kisii Campus");
+  console.log('New Department Created:', newDepartment);
+}
