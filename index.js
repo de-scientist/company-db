b65e187dfb7096d9e2b9bfaa0bf74566ac746b85
de-scientist <gitaumark502@gmail.com>
2025-10-22 12:05:44 +0300

    Call the updateDepartment function to change the location of DPT004 and log the result

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ main()
       console.log('Deleted Department:', deletedDepartment);
     });
 
-    
+
     //Write a function named updateDepartment that updates the location of the Cyber Security department (DPT004) to "Kericho Campus".The function should use Prisma's update() method to find the department by its ID (DPT004) and modify the location field.
     async function updateDepartment(departmentId, newLocation) {
       const updatedDepartment = await prisma.department.update({
@@ -98,6 +98,7 @@ main()
       return updatedDepartment;
     }
 
+    
     // Call the updateDepartment function to change the location of DPT004 and log the result
     const updateId = "DPT004";
     const newLocation = "Kericho Campus";
